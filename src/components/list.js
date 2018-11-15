0158e5aebaba8627893d278fb315b1d0cbdfb7f7
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-11-15 11:14:31 +0200

    add features/make HW
    delete "items" array
    add "name" props to child "Name"

--- src/components/list.js
+++ src/components/list.js
@@ -1,41 +1,23 @@
-import React, { Component} from 'react';
-import Name from "./name";
+import React, { Component } from 'react';
+import Name from "./name"
 
-class List extends Component {
-  render () {
-    const items = [
-      {
-        name: 'Stas',
-        show: true
-      },
-      {
-        name: 'Andrian',
-        show: true
-      },
-      {
-        name: 'Olga',
-        show: false
-      },
-      {
-        name: 'Nazar',
-        show: true
-      }
-    ];
+class List extends Component{
+    render() {      
 
-    const viewItems = items
-        .map((i, index) => <Name item={i} key={index} />);
-    console.log('List in render');
+    const viewItems = this.props.items
+    .map((i,index) => <Name  item= {i} name={i.name} key={index}  />);
+    console.log("List in render");
+
     return (<div className="list">
-      Component List
-      { this.props.children }
-      <div>
-        Items:
-        <ul>
-          {viewItems}
-        </ul>
-      </div>
-    </div>);
-  }
-}
+        Component list
+            <div>
+                Items:
+                <ul>
+                    {viewItems}
+                </ul>
+            </div>
+        </div>);
+    }
+};
 
-export default List;
+export default  List
