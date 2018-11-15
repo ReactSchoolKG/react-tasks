e9f2b0da4a81a8aef7bd28c358f40ef4f7aa3b1f
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-11-15 11:11:59 +0200

    Add features
    create "-" button to decrease counter
    crate items array and throw it for child in props

--- src/components/example.js
+++ src/components/example.js
@@ -1,40 +1,59 @@
-import React, { Component} from 'react';
-import List from "./list";
+import React, { Component } from 'react';
+import List from "./list"
 
-class Example extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
+class Example extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            counter:0
+        };
+        this._hendleClickP = this._hendleClickP.bind(this);
+    }
+    render() {
+        const items =[{
+            name: "Stas",
+            show:true},
+        {
+            name: "Dan",
+            show:false},
+        {
+            name: "Ross",
+            show:true
+        },
+        {
+            show:true}
+    ];
+        console.log("Example in render");
+        return (<div className="Example">
+        Component Example
+        <div className="list">
+            List
+            <List items = {items}>
+                <h2>Information from Example</h2>
+                <h3>Test</h3>
+                List
+            </List>
+        </div>
+        <h1>Counter</h1>
+        <div>  
+            <button onClick={()=>this._hendleClickM()}>-</button>
+            {this.state.counter}
+            <button onClick={this._hendleClickP}>+</button>
+        </div>
+        </div>);
+    }
+    _hendleClickP(e){
+        this.setState({
+            counter: this.state.counter + 1
+        });
     };
-  }
-  render () {
-    console.log('Example in render');
-    return (<div className="example">
-      Component Example
-      <div>
-        <List>
-          <h2>
-            Information from Example
-          </h2>
-          <h3>
-            Test
-          </h3>
-        </List>
-      </div>
-      <h1>Counter</h1>
-      <div>
-        { this.state.counter }
-        <button onClick={this._handleClick} >+</button>
-      </div>
-    </div>);
-  }
+    _hendleClickM(e){
+        if(this.state.counter){
+            this.setState({
+                counter: this.state.counter - 1 
+            });
+        }   
+    }
+};
 
-  _handleClick = e => {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
-}
-
-export default Example;
+export default  Example
