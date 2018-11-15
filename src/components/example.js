5bd4d0ec15392a81d8c0336da0e38544f07afda2
Tania Patsai <tpacaj2301@gmail.com>
2018-11-15 16:02:23 +0200

    task1-react

--- src/components/example.js
+++ src/components/example.js
@@ -1,19 +1,41 @@
 import React, { Component} from 'react';
 import List from "./list";
 
+
 class Example extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0
     };
+    this.items = [
+      {
+        name: 'Stas',
+        show: true
+      },
+      {
+        name: 'Andrian',
+        show: true
+      },
+      {
+        name: 'Olga',
+        show: false
+      },
+      {
+        name: 'Nazar',
+        show: true
+      },
+      {
+          show: true
+        }
+    ];
   }
   render () {
     console.log('Example in render');
     return (<div className="example">
       Component Example
       <div>
-        <List>
+        <List items={this.items}>
           <h2>
             Information from Example
           </h2>
--- src/components/list.js
+++ src/components/list.js
@@ -3,24 +3,7 @@ import Name from "./name";
 
 class List extends Component {
   render () {
-    const items = [
-      {
-        name: 'Stas',
-        show: true
-      },
-      {
-        name: 'Andrian',
-        show: true
-      },
-      {
-        name: 'Olga',
-        show: false
-      },
-      {
-        name: 'Nazar',
-        show: true
-      }
-    ];
+    const {items} = this.props;
 
     const viewItems = items
         .map((i, index) => <Name item={i} key={index} />);
