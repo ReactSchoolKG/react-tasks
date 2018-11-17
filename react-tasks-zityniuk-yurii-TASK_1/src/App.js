df49f871aeea2256119415c2cfd3848c85e00436
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-11-17 17:31:44 +0200

    update Apps

--- react-tasks-zityniuk-yurii-TASK_1/src/App.js
+++ react-tasks-zityniuk-yurii-TASK_1/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import Example from './components/example';
+// import Example from './components/example';
+// import Counter from './components/counter/counter';
+import Tick from "./components/tick/tick";
+
 
 import './App.css';
 
@@ -7,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Example />
+        <Tick/>
+        {/* <Example />        */}
+        {/* <Counter/> */}
       </div>
     );
   }
