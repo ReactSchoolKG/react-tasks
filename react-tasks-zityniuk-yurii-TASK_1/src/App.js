0b545eb2b8fdf0776f3f8b85737edc167f8ffc2e
KeinOhrHasen <34781917+KeinOhrHasen@users.noreply.github.com>
2018-11-24 12:21:33 +0200

    React HomeTask #3

--- react-tasks-zityniuk-yurii-TASK_1/src/App.js
+++ react-tasks-zityniuk-yurii-TASK_1/src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
+import './App.css';
 // import Example from './components/example';
 // import Counter from './components/counter/counter';
-import Tick from "./components/tick/tick";
+// import Tick from "./components/tick/tick";
+import Clock from "./components/hoc/clock";
+import WithClock from "./components/hoc/withClock";
 
+const WrappedComp = WithClock (Clock);
 
-import './App.css';
-
-class App extends Component {
+class App extends Component { 
   render() {
     return (
-      <div className="App">
-        <Tick/>
-        {/* <Example />        */}
-        {/* <Counter/> */}
-      </div>
+        <div className="App">
+        <Suspense fallback={<div>Loading...</div>}>
+           <WrappedComp></WrappedComp>
+           <WrappedComp></WrappedComp> 
+          {/* <Clock></Clock> */}
+          {/* <Tick/> */}
+          {/* <Example />        */}
+          {/* <Counter/> */}
+          </Suspense>
+        </div>
     );
   }
 }
+
 
 export default App;
